be9d7313d2250187ceb659bb7b71caa239dcfeeb
Chris Needham <chris.needham@bbc.co.uk>
2021-02-18 23:33:55 +0000

    Moved hexdump to cmake.yml

--- src/StdioTest.cpp
+++ src/StdioTest.cpp
@@ -5,7 +5,5 @@ int main(int argc, char** argv)
     const char* command = "./audiowaveform -i ../test/data/test_file_stereo.mp3 -o temp.json -b 8 1>stdout.txt 2>stderr.txt";
     system(command);
 
-    system("hexdump -C stderr.txt");
-
     return 0;
 }
